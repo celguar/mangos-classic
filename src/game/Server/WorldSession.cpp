1df3503fd5889017e4c8f6def443232e4eb8e1b7
celguar <celguar@gmail.com>
2022-05-10 13:37:18 +0300

    WIP anti crash & instances speed up
    Port from Sunstrider Core
    Kick and save on map crash and try to restore
    Speed up instances update (disabled)
    
    Allow kicking to character screen without closing session

--- src/game/Server/WorldSession.cpp
+++ src/game/Server/WorldSession.cpp
@@ -855,12 +855,12 @@ void WorldSession::LogoutPlayer()
 }
 
 /// Kick a player out of the World
-void WorldSession::KickPlayer(bool save, bool inPlace)
+void WorldSession::KickPlayer(bool save, bool inPlace, bool kickSession)
 {
     if (inPlace)
     {
         m_playerSave = save;
-        m_kickSession = true;
+        m_kickSession = kickSession;
         LogoutPlayer();
         return;
     }
