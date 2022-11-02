20fcbe4525135584d27ea3e3c5748a8fcfef2b41
celguar <celguar@gmail.com>
2022-11-03 02:28:28 +0300

    Less often far distance update

--- src/game/Maps/Map.cpp
+++ src/game/Maps/Map.cpp
@@ -1294,10 +1294,6 @@ void Map::Remove(Player* player, bool remove)
     SendRemoveTransports(player);
     UpdateObjectVisibility(player, cell, p);
 
-#ifdef ENABLE_PLAYERBOTS
-    if (!player->GetPlayerbotAI())
-        player->ResetMap();
-#else
     player->ResetMap();
 
     if (remove)
