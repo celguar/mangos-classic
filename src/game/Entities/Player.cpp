c5eda5dc332e5eb34eba107f607d43021e77259c
mostlikely4r <sebastiaan.keek@gmail.com>
2022-11-13 22:21:46 +0100

    -Revert not loading social for bots until further notice

--- src/game/Entities/Player.cpp
+++ src/game/Entities/Player.cpp
@@ -14498,18 +14498,7 @@ bool Player::LoadFromDB(ObjectGuid guid, SqlQueryHolder* holder)
     _LoadActions(holder->GetResult(PLAYER_LOGIN_QUERY_LOADACTIONS));
     _LoadForgottenSkills(holder->GetResult(PLAYER_LOGIN_QUERY_FORGOTTEN_SKILLS));
 
-#ifdef ENABLE_PLAYERBOTS
-    if (isRealPlayer())
     m_social = sSocialMgr.LoadFromDB(holder->GetResult(PLAYER_LOGIN_QUERY_LOADSOCIALLIST), GetObjectGuid());
-    else
-    {
-#endif
-
-        m_social = new PlayerSocial();
-        m_social->SetPlayerGuid(GetObjectGuid());
-#ifdef ENABLE_PLAYERBOTS
-    }
-#endif
 
     Taxi::DestID destOrphan = 0;
     if (!m_taxiTracker.Load(taxi_nodes, destOrphan))
