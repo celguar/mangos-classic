005a9ec1580a47a8d4d3f1189ef391b6e4f32172
mostlikely4r <sebastiaan.keek@gmail.com>
2022-11-28 10:44:07 +0100

    -Visibiltiy fix: Fix bots getting default visbility instead of that defined in config

--- src/game/Maps/Map.cpp
+++ src/game/Maps/Map.cpp
@@ -1045,10 +1045,22 @@ void Map::Update(const uint32& t_diff)
             continue;
 
 #ifdef ENABLE_PLAYERBOTS
-        if (!player->isRealPlayer())
-            player->GetVisibilityData().SetVisibilityDistanceOverride(VisibilityDistanceType::Tiny);
-#endif
+        if (!player->isRealPlayer()) //For non-players only load the grid.
+        {
+            CellPair center = MaNGOS::ComputeCellPair(player->GetPositionX(), player->GetPositionY()).normalize();
+            uint32 cell_id = (center.y_coord * TOTAL_NUMBER_OF_CELLS_PER_MAP) + center.x_coord;
 
+            if (!isCellMarked(cell_id))
+            {
+                Cell cell(center);
+                const uint32 x = cell.GridX();
+                const uint32 y = cell.GridY();
+                if (!cell.NoCreate() || loaded(GridPair(x, y)))
+                    EnsureGridLoaded(player->GetCurrentCell());
+            }
+            continue;
+        }
+#endif
         // update objects beyond visibility distance
         if (!player->GetPlayerbotAI() && !player->isAFK() && !player->IsStopped() && !urand(0, 9))
             player->GetCamera().UpdateVisibilityForOwner(false, true);
@@ -1058,11 +1070,6 @@ void Map::Update(const uint32& t_diff)
         // If player is using far sight, visit that object too
         if (WorldObject* viewPoint = GetWorldObject(player->GetFarSightGuid()))
             VisitNearbyCellsOf(viewPoint, grid_object_update, world_object_update);
-
-#ifdef ENABLE_PLAYERBOTS
-        if (!player->isRealPlayer())
-            player->GetVisibilityData().SetVisibilityDistanceOverride(VisibilityDistanceType::Normal);
-#endif
     }
 
     // non-player active objects
