990ca65c04fb74fde00d4cab2776691e4629cc84
mostlikely4r <sebastiaan.keek@gmail.com>
2023-01-01 00:43:01 +0100

    -Internal: Fixes for Linux compilation.

--- src/game/Maps/Map.cpp
+++ src/game/Maps/Map.cpp
@@ -1027,6 +1027,7 @@ void Map::Update(const uint32& t_diff)
             }
         }
     }
+#endif
 
     // Reset the has real players flag and check for it again
     const bool hadRealPlayers = hasRealPlayers;
@@ -1118,7 +1119,6 @@ void Map::Update(const uint32& t_diff)
         sLog.outBasic("Map %u: Active Zone Players - %u of %u", GetId(), activePlayers, m_mapRefManager.getSize());
     }
 #endif
-
     for (m_mapRefIter = m_mapRefManager.begin(); m_mapRefIter != m_mapRefManager.end(); ++m_mapRefIter)
     {
         Player* player = m_mapRefIter->getSource();
@@ -1169,6 +1169,7 @@ void Map::Update(const uint32& t_diff)
 
     // non-player active objects
     bool updateObj = urand(0, (HasRealPlayers() ? avgDiff : (avgDiff * 3))) < 10;
+#endif
     if (!m_activeNonPlayers.empty())
     {
         for (m_activeNonPlayersIter = m_activeNonPlayers.begin(); m_activeNonPlayersIter != m_activeNonPlayers.end();)
@@ -1194,7 +1195,6 @@ void Map::Update(const uint32& t_diff)
                 }
             }
 #endif
-
             objToUpdate.insert(obj);
 
             // lets update mobs/objects in ALL visible cells around player!
