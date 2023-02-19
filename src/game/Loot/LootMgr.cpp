a195d87050dd40fd978bedea4ae174b74a8f2845
Flekz <davidparraausina@gmail.com>
2023-02-19 11:47:47 +0000

    Enhance the system to keep gear enchants and durability, and there is no need to add the hardcore loot to the gameobject_loot_template table anymore

--- src/game/Loot/LootMgr.cpp
+++ src/game/Loot/LootMgr.cpp
@@ -31,6 +31,7 @@
 #include "Tools/Language.h"
 #include <sstream>
 #include <iomanip>
+#include "Hardcore/HardcoreMgr.h"
 
 INSTANTIATE_SINGLETON_1(LootMgr);
 
@@ -870,7 +871,7 @@ void Loot::AddItem(LootStoreItem const& item)
 }
 
 // Insert item into the loot explicit way. (used for container item and Item::LoadFromDB)
-void Loot::AddItem(uint32 itemid, uint32 count, uint32 randomSuffix, int32 randomPropertyId)
+void Loot::AddItem(uint32 itemid, uint32 count, uint32 randomSuffix, int32 randomPropertyId, bool addPermissions)
 {
     if (m_lootItems.size() < MAX_NR_LOOT_ITEMS)                              // Normal drop
     {
@@ -879,8 +880,11 @@ void Loot::AddItem(uint32 itemid, uint32 count, uint32 randomSuffix, int32 rando
         m_lootItems.push_back(lootItem);
 
         // add permission to pick this item to loot owner
-        for (auto allowedGuid : m_ownerSet)
-            lootItem->allowedGuid.emplace(allowedGuid);
+        if (addPermissions)
+        {
+            for (auto allowedGuid : m_ownerSet)
+                lootItem->allowedGuid.emplace(allowedGuid);
+        }
     }
 }
 
@@ -891,18 +895,21 @@ bool Loot::FillLoot(uint32 loot_id, LootStore const& store, Player* lootOwner, b
     if (!lootOwner)
         return false;
 
-    LootTemplate const* tab = store.GetLootFor(loot_id);
-
-    if (!tab)
+    if (!sHardcoreMgr.FillLoot(*this))
     {
-        if (!noEmptyError)
-            sLog.outErrorDb("Table '%s' loot id #%u used but it doesn't have records.", store.GetName(), loot_id);
-        return false;
+        LootTemplate const* tab = store.GetLootFor(loot_id);
+
+        if (!tab)
+        {
+            if (!noEmptyError)
+                sLog.outErrorDb("Table '%s' loot id #%u used but it doesn't have records.", store.GetName(), loot_id);
+            return false;
+        }
+
+        m_lootItems.reserve(MAX_NR_LOOT_ITEMS);
+
+        tab->Process(*this, lootOwner, store, store.IsRatesAllowed()); // Processing is done there, callback via Loot::AddItem()
     }
-
-    m_lootItems.reserve(MAX_NR_LOOT_ITEMS);
-
-    tab->Process(*this, lootOwner, store, store.IsRatesAllowed()); // Processing is done there, callback via Loot::AddItem()
 
     // fill the loot owners right here so its impossible from this point to change loot result
     Player* masterLooter = nullptr;
@@ -2031,6 +2038,8 @@ InventoryResult Loot::SendItem(Player* target, LootItem* lootItem, bool sendErro
         if (msg == EQUIP_ERR_OK)
         {
             Item* newItem = target->StoreNewItem(dest, lootItem->itemId, true, lootItem->randomPropertyId);
+
+            sHardcoreMgr.OnItemLooted(*this, newItem, target);
 
             if (lootItem->freeForAll)
             {
