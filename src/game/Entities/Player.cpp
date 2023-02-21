47f9a2989c68942a0ab923e5e9f22d174f1d0565
Flekz <davidparraausina@gmail.com>
2023-02-21 16:13:58 +0000

    Allow bots to drop money, items or gear

--- src/game/Entities/Player.cpp
+++ src/game/Entities/Player.cpp
@@ -1720,8 +1720,6 @@ void Player::SetDeathState(DeathState s)
 
         if (InstanceData* mapInstance = GetInstanceData())
             mapInstance->OnPlayerDeath(this);
-
-        sHardcoreMgr.OnPlayerDeath(this);
     }
 
     Unit::SetDeathState(s);
