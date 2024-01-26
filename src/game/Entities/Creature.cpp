e6655b3a2d3b5f7a2bfeeff66ad032a1f3949d28
Flekz <davidparraausina@gmail.com>
2024-01-26 12:06:36 +0000

    Add disable instance respawn

--- src/game/Entities/Creature.cpp
+++ src/game/Entities/Creature.cpp
@@ -52,6 +52,10 @@
 // apply implementation of the singletons
 #include "Policies/Singleton.h"
 
+#ifdef ENABLE_IMMERSIVE
+#include "Immersive.h"
+#endif
+
 TrainerSpell const* TrainerSpellData::Find(uint32 spell_id) const
 {
     TrainerSpellMap::const_iterator itr = spellList.find(spell_id);
@@ -718,6 +722,13 @@ void Creature::Update(const uint32 diff)
         {
             if (m_respawnTime <= time(nullptr) && (!m_isSpawningLinked || GetMap()->GetCreatureLinkingHolder()->CanSpawn(this)))
             {
+#ifdef ENABLE_IMMERSIVE
+                if (!sImmersive.CanCreatureRespawn(this))
+                    return;
+
+                m_manualRespawn = false;
+#endif
+
                 DEBUG_FILTER_LOG(LOG_FILTER_AI_AND_MOVEGENSS, "Respawning...");
                 m_respawnTime = 0;
                 SetCanAggro(false);
@@ -1945,6 +1956,10 @@ void Creature::Respawn()
         if (HasStaticDBSpawnData())
             GetMap()->GetPersistentState()->SaveCreatureRespawnTime(GetDbGuid(), 0);
         m_respawnTime = time(nullptr);                         // respawn at next tick
+
+#ifdef ENABLE_IMMERSIVE
+        m_manualRespawn = true;
+#endif
     }
 }
 
