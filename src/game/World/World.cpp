54aa1be238c26dc2d1243c18c195e185e6867f93
David Parra <davidparraausina@gmail.com>
2024-02-09 19:55:16 +0000

    Merge db (#91)

--- src/game/World/World.cpp
+++ src/game/World/World.cpp
@@ -2379,7 +2379,6 @@ void World::UpdateResultQueue()
     CharacterDatabase.ProcessResultQueue();
     WorldDatabase.ProcessResultQueue();
     LoginDatabase.ProcessResultQueue();
-    PlayerbotDatabase.ProcessResultQueue();
 }
 
 void World::UpdateRealmCharCount(uint32 accountId)
