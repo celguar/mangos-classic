53202c9ad1161753add7070c7ac32f5acff819af
celguar <celguar@gmail.com>
2024-03-29 20:01:50 +0300

    Fix merge

--- src/game/Entities/Player.cpp
+++ src/game/Entities/Player.cpp
@@ -19659,7 +19659,6 @@ void Player::learnSpellHighRank(uint32 spellid)
     sSpellMgr.doForHighRanks(spellid, worker);
 }
 
-#ifdef ENABLE_PLAYERBOTS
 void Player::learnClassLevelSpells(bool includeHighLevelQuestRewards)
 {
     ChrClassesEntry const* clsEntry = sChrClassesStore.LookupEntry(getClass());
@@ -19814,7 +19813,6 @@ void Player::learnClassLevelSpells(bool includeHighLevelQuestRewards)
         }
     }
 }
-#endif
 
 void Player::_LoadSkills(std::unique_ptr<QueryResult> queryResult)
 {
--- src/game/Entities/Player.h
+++ src/game/Entities/Player.h
@@ -1497,9 +1497,7 @@ class Player : public Unit
         void learnQuestRewardedSpells();
         void learnQuestRewardedSpells(Quest const* quest);
         void learnSpellHighRank(uint32 spellid);
-#ifdef ENABLE_PLAYERBOTS
         void learnClassLevelSpells(bool includeHighLevelQuestRewards = false);
-#endif
 
         uint32 GetFreeTalentPoints() const { return GetUInt32Value(PLAYER_CHARACTER_POINTS1); }
         void SetFreeTalentPoints(uint32 points) { SetUInt32Value(PLAYER_CHARACTER_POINTS1, points); }
